3a9f759a86f4bb34b6450b67474375d7c43de889
FerroPrata <152465906+FerroPrata@users.noreply.github.com>
2023-12-17 10:46:08 +0700

    Update data.py
    penambahan tampungan kode

--- algo/perpus/data.py
+++ algo/perpus/data.py
@@ -3,29 +3,29 @@
 import json
 
 class Tes:
-    def __init__(self, file_path='/backup data 2023/optional/belajar python/algo/perpus/data_buku.json'):
+    def __init__(self, file_path='\\Users\\Lenovo\\Documents\\Algoritma Dasar\\Project 1\\data_buku.json'):
         try:
             with open(file_path, 'r') as file:
                 self.buku = json.load(file)
         except (FileNotFoundError, json.JSONDecodeError):
             self.buku = {}
-    def __inti__(self, file_path='/backup data 2023/optional/belajar python/algo/perpus/bukti_user.json'):
+    def __inti__(self, file_path='\\Users\\Lenovo\\Documents\\Algoritma Dasar\\Project 1\\bukti_user.json'):
         try:
             with open(file_path, 'r') as file:
                 self.bukti = json.load(file)
         except (FileNotFoundError, json.JSONDecodeError):
             self.bukti = {}
 
-    def save_to_file(self, file_path='/backup data 2023/optional/belajar python/algo/perpus/data_buku.json'):
+    def save_to_file(self, file_path='\\Users\\Lenovo\\Documents\\Algoritma Dasar\\Project 1\\data_buku.json'):
         with open(file_path, 'w') as file:
             json.dump(self.buku, file, indent=2)
 
-    def tambah_buku(self, judul, total, kategori, thn_terbit, penulis, penerbit, jumlah_hal):
-        if judul not in self.buku:
-            self.buku[judul] = {"total": total, "tersedia": total, "kategori": kategori, "tahun_terbit": thn_terbit, "Penulis": penulis, "Penerbit": penerbit, "jumlah_halaman": jumlah_hal }
-            print(f"Buku {judul} berhasil ditambahkan.")
-        else:
-            print(f"Buku dengan judul {judul} sudah ada.")
+    # def tambah_buku(self, kode , judul, total, kategori, thn_terbit, penulis, penerbit, jumlah_hal):
+    #     if judul not in self.buku:
+    #         self.buku[judul] = {"kode": kode, "total": total, "tersedia": total, "kategori": kategori, "tahun_terbit": thn_terbit, "Penulis": penulis, "Penerbit": penerbit, "jumlah_halaman": jumlah_hal }
+    #         print(f"Buku {judul} berhasil ditambahkan.")
+    #     else:
+    #         print(f"Buku dengan judul {judul} sudah ada.")
     
     def filter_buku_by_kategori(self, kategori):
         hasil_filter = {judul: buku_info for judul, buku_info in self.buku.items() if buku_info.get("kategori") == kategori}
