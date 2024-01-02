a742433a2138fdb006dfe6063d88ce98269548b3
FerroPrata <152465906+FerroPrata@users.noreply.github.com>
2024-01-02 13:20:24 +0700

    Update patch1.py
    sorting_by_rating

--- algo/sorting/patch1.py
+++ algo/sorting/patch1.py
@@ -2,10 +2,10 @@ import json
 import time
 from prettytable import PrettyTable
 
-with open("D:/backup data 2023/optional/belajar python/algo/sorting/data.json", "r") as f:
+with open("\\Users\\Lenovo\\Documents\\Algoritma Dasar\\Tugas AKhir\\data.json", "r") as f:
     dataa = json.load(f)
 
-tahun_terbit_list = [buku["tahun_terbit"] for buku in dataa.values()]
+rating = [buku["rating"] for buku in dataa.values()]
 nama = [buku for buku in dataa.keys()]
 
 
@@ -17,11 +17,11 @@ def insertion_sort(data):
     for i in range(1, len(data)):  
         key_item = data[i]  
         j = i - 1  
-        while j >= 0 and data[j][1]["tahun_terbit"] > key_item[1]["tahun_terbit"]:  
+        while j >= 0 and data[j][1]["rating"] < key_item[1]["rating"]:  
             data[j + 1] = data[j]  
             j -= 1
         data[j + 1] = key_item  
-        print(f"step ke-{a} : {[buku[1]['tahun_terbit'] for buku in data]}, iterasi ke-{i}")
+        print(f"step ke-{a} : {[buku[1]['rating'] for buku in data]}, iterasi ke-{i}")
         a += 1
 
     waktuS = time.time()
@@ -32,20 +32,20 @@ def insertion_sort(data):
 
 sorted_data = insertion_sort(data)
 
-print("\nbuku sebelum di sorting")
-tabel = list(zip(nama, tahun_terbit_list))
+print("\nWarung sebelum di sorting")
+tabel = list(zip(nama, rating))
 
 table1 = PrettyTable()
-table1.field_names = ["nama buku", "tahun terbit"]
+table1.field_names = ["nama warung", "rating"]
 for buku in tabel:
     table1.add_row(buku)
 print(table1)
 
-print("\nbuku setelah di sorting by tahun")
+print("\nWarung setelah di sorting by rating")
 
 table = PrettyTable()
-table.field_names = ["nama buku", "tahun terbit"]
+table.field_names = ["nama warung", "rating"]
 for buku in sorted_data:
-    table.add_row([buku[0], buku[1]['tahun_terbit']])
+    table.add_row([buku[0], buku[1]['rating']])
 print(table)
 
