135d113062e21a1f13f11e1daa76f424fcc2a725
FerroPrata <152465906+FerroPrata@users.noreply.github.com>
2024-01-14 12:33:01 +0700

    Update patch3.py
    terbaru

--- algo/sorting/patch3.py
+++ algo/sorting/patch3.py
@@ -1,7 +1,7 @@
 import json
 from prettytable import PrettyTable
 import datetime
-
+import time
 
 def selection_sort():
     global data
@@ -13,20 +13,26 @@ def selection_sort():
     now = datetime.datetime.now().time()
     n = len(data)
     a = 0
+    waktuM = time.time()
     for i in range(n - 1):
         max_idx = i
         for j in range(i + 1, n):
             if data[j][1]["rekomendasi"] > data[max_idx][1]["rekomendasi"]:
                 max_idx = j
-                data[i], data[max_idx] = data[max_idx], data[i]
-                print(f"step ke-{a + 1} : {[warung[1]['rekomendasi'] for warung in data]}, iterasi ke-{i+1}")
-                a += 1
-def helo():  
-    global data
-    global now 
-    table = PrettyTable()
-    table.field_names = ["Nama warung", "rekomendasi", "rating","jarak", "status", "jam buka", "jam tutup"]
+        data[i], data[max_idx] = data[max_idx], data[i]
+        print(f"step ke-{a + 1} : {[warung[1]['rekomendasi'] for warung in data]}, iterasi ke-{i+1}")
+        a += 1
+        if a == n - 1:
+            break
+    waktuS = time.time()
+    waktuA = waktuS - waktuM
+    print(f'Waktu : {waktuA:.6f}')
 
+def coba1():
+    global hasil
+    global data
+    selection_sort()
+    result_list = []
     for warung in data:
         jam_buka = datetime.datetime.strptime(warung[1]['jam buka'], "%H:%M").time()
         jam_tutup = datetime.datetime.strptime(warung[1]['jam tutup'], "%H:%M").time()
@@ -34,13 +40,43 @@ def helo():
             status = "buka"
         else:
             status = "tutup"
-        jrk = warung[1]['jarak']
-        if jrk > 1000:
-            jarak = f"{jrk / 1000} KM"
-        else:
-            jarak = f"{jrk} M"
-        table.add_row([warung[0], "{:.1f}".format(warung[1]['rekomendasi']), warung[1]["rating"], jarak, status, warung[1]['jam buka'], warung[1]["jam tutup"]])
+        result_list.append({
+            "Nama warung": warung[0],
+            "rekomendasi": warung[1]['rekomendasi'],
+            "status": status,
+            "jam buka": warung[1]['jam buka'],
+            "jam tutup": warung[1]['jam tutup']
+        })
+        
+    hasil = result_list
 
+def selection_sort2():
+    coba1()
+    global data
+    global now
+    data = list(hasil)
+    now = datetime.datetime.now().time()
+    n = len(data)
+    a = 0
+    waktuM = time.time()
+    for i in range(n - 1):
+        max_idx = i
+        for j in range(i + 1, n):
+            if data[j]["status"] < data[max_idx]["status"]:
+                max_idx = j
+        data[i], data[max_idx] = data[max_idx], data[i]
+        print(f"step ke-{a + 1} : {[warung['status'] for warung in data]}, iterasi ke-{i+1}")
+        a += 1
+        if a == n - 1:
+            break
+    waktuS = time.time()
+    waktuA = waktuS - waktuM
+    print(f'Waktu : {waktuA:.6f}')
 
-
+def coba2():
+    selection_sort2()
+    table = PrettyTable()
+    table.field_names = ["Nama warung", "rekomendasi", "status", "jam buka", "jam tutup"]
+    for warung in data:
+        table.add_row([warung["Nama warung"], "{:.1f}".format( warung['rekomendasi']), warung['status'], warung['jam buka'], warung['jam tutup']])
     print(table)
